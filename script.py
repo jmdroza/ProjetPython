546f0f3878df81626dcd08ab0ca505bd09a6f4fe
jmdroza <droza.jessica@gmail.com>
2021-07-22 09:52:51 +0200

    Récupération donnée des capteurs

--- script.py
+++ script.py
@@ -1,20 +1,20 @@
 import json
 import requests
+import meteo
+from tinydb import TinyDB, Query
 
 def function():
     url = "http://app.objco.com:8099/?account=1IF0PIBK37&limit=5"
     # Get dummy data using an API
     reponse = requests.get(url)
-    print(reponse)
 
-    # Convert data to dict
+    # loads : create a json object
     data = json.loads(reponse.text)
-
-    # Convert dict to string
-    data = json.dumps(data)
-
-    print(data)
-    print(type(data))
-    return data
+    list_meteo = meteo.create_meteo_object_from_data(data)
+    res = ""
+    for x in list_meteo :
+        res += meteo.toString(x) + "\n"
+    print(res)
+    return res
 
 function()
